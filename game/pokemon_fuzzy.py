56120d0f7584c36047ea6f9df21a910f785b75e6
Gonçalo Sobral <gfgsl@iscte-iul.pt>
2026-03-21 21:12:07 +0000

    começo do fuzzy

--- game/pokemon_fuzzy.py
+++ game/pokemon_fuzzy.py
@@ -6,9 +6,16 @@ from skfuzzy import control as ctrl
 Cria um sistema Fuzzy que recebe como input a diferença dos niveis
 e o efeito do ataque e devolve como input a probabilidade de ganhar
 '''
+#Antecedentes
+lvl_diff = ctrl.Antecedent(np.arange(-9, 10, 1), 'lvl_diff')
+effect = ctrl.Antecedent(np.arange(0, 4.1, 0.1), 'effect')
+
+#Consequent
+probabilidade = ctrl.Consequent(np.arange(0, 1.0, 0.01), 'probabilidade')
+
+
 
 def calculate_prob(level_input, effect_input):
-    # TO DO
     return 1
     
 
